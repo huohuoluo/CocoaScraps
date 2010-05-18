2040dc8e36ecdc50fb2e7815b5cab4fab0409914
Akshay Rawat <projects@akshay.cc>
2010-05-18 19:57:33 +0530

    those rovers

---
+++ raiseman/PersonTest.h
@@ -0,0 +1,16 @@
+//
+//  PersonTest.h
+//  raiseman
+//
+//  Created by Akshay on 5/18/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface PersonTest : SenTestCase {
+
+}
+
+@end
---
+++ raiseman/PersonTest.m
@@ -0,0 +1,15 @@
+//
+//  PersonTest.m
+//  raiseman
+//
+//  Created by Akshay on 5/18/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "PersonTest.h"
+#import "Person.h"
+
+@implementation PersonTest
+
+-(void)test
+@end
