2177fc58fbb32486be16fd46cd5d5e441820abd5
Akshay Rawat <projects@akshay.cc>
2010-05-24 22:13:11 +0530

    testing infrastructure

---
+++ GrokkingRecipes/RecipeTest.h
@@ -0,0 +1,16 @@
+//
+//  RecipeTest.h
+//  GrokkingRecipes
+//
+//  Created by Akshay on 5/24/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface RecipeTest : SenTestCase {
+
+}
+
+@end
---
+++ GrokkingRecipes/RecipeTest.m
@@ -0,0 +1,19 @@
+//
+//  RecipeTest.m
+//  GrokkingRecipes
+//
+//  Created by Akshay on 5/24/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "RecipeTest.h"
+
+
+@implementation RecipeTest
+
+-(void)testTruth
+{
+	STAssertTrue(true, @"the truth never lies");
+}
+
+@end
